d29c97f7ed831ac09829a2c0913b2d850827a3fb
Twitch0125 <twitcherc@gmail.com>
2019-09-04 08:25:41 -0600

    chat type

---
+++ src/app/interfaces/chat.ts
@@ -0,0 +1,4 @@
+export interface Chat {
+  members: string[];
+  id: string; //uuid v4
+}
--- src/app/services/chat-list.service.ts
+++ src/app/services/chat-list.service.ts
@@ -1,9 +1,24 @@
+import { Chat } from '../interfaces/chat';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatListService {
+  chats: Chat[];
+  constructor() {}
 
-  constructor() { }
+  getChats(): Chat[] {
+    return this.chats;
+  }
+
+  private setChats(newArray: Chat[]) {
+    this.chats = newArray;
+  }
+  addChat(newChat: Chat) {
+    this.chats.push(newChat);
+  }
+  removeChat(chat: Chat) {
+    this.setChats(this.chats.filter(chatItem => chatItem.id != chat.id));
+  }
 }
