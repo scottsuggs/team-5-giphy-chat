7bb83f17357f64f770f1fe11533342b9748c9cdd
Kaleb Ercanbrack <kaleb@localhost.localdomain>
2019-09-12 10:11:57 -0600

    creating chats

--- src/app/add-chat/add-chat.page.ts
+++ src/app/add-chat/add-chat.page.ts
@@ -1,13 +1,6 @@
-import {
-  AfterContentInit,
-  AfterViewChecked,
-  Component,
-  DoCheck,
-  OnInit
-} from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { FirebaseService } from '../services/firebase.service';
-import { Observable } from 'rxjs';
 import { User } from '../interfaces/user';
 
 @Component({
@@ -15,56 +8,27 @@ import { User } from '../interfaces/user';
   templateUrl: './add-chat.page.html',
   styleUrls: ['./add-chat.page.scss']
 })
-export class AddChatPage implements OnInit, AfterContentInit, DoCheck {
+export class AddChatPage implements OnInit, OnDestroy {
   users: User[];
   query: string;
+  subscription;
 
   constructor(private firebase: FirebaseService) {}
 
   ngOnInit() {
-    this.firebase.getUsers().subscribe(data => {
-      this.users = data;
-      console.log('init users', this.users);
-    });
+    this.subscription = this.firebase.getUsers().subscribe(
+      data => {
+        this.users = data;
+      },
+      err => console.log(err)
+    );
   }
 
-  ngDoCheck(): void {
-    //Called every time that the input properties of a component or a directive are checked. Use it to extend change detection by performing a custom check.
-    //Add 'implements DoCheck' to the class.
-    this.firebase.getUsers().subscribe(data => {
-      this.users = data;
-      console.log('init users', this.users);
-    });
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
-  ionViewWillEnter(): void {
-    // this.firebase.getUsers().subscribe(data => {
-    //   this.users = data;
-    //   console.log('init users', this.users);
-    // });
-  }
-
-  ionViewDidEnter(): void {
-    // this.firebase.getUsers().subscribe(data => {
-    //   this.users = data;
-    //   console.log('init users', this.users);
-    // });
-  }
-
-  ngAfterContentInit(): void {
-    //Called after ngOnInit when the component's or directive's content has been initialized.
-    //Add 'implements AfterContentInit' to the class.
-    // this.firebase.getUsers().subscribe(data => {
-    //   this.users = data;
-    //   console.log('init users', this.users);
-    // });
-  }
-
-  log() {
-    console.log('users', this.users);
-  }
-
-  addUser(user: User) {
-    // this.firebase.addUserDocument(user);
+  addChat(user: User) {
+    this.firebase.addChatDocument();
   }
 }
